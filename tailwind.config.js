775799170b130bc87a60261bc15848611a2c166b
Vazman117 <vazman117@gmail.com>
2025-11-26 12:57:24 -0600

    yA FUNCIONA, QUIERO QUE FUNCIONES

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,14 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  root: 'client', // IMPORTANTE: le decimos a Vite que el proyecto está en client
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'client','src'), // Esto hace que @ apunte a /src
+      '@': path.resolve(__dirname, 'client', 'src'), // ahora apunta bien
     },
+  },
+  build: {
+    outDir: 'dist', // salida dentro de client/dist
   },
 });
